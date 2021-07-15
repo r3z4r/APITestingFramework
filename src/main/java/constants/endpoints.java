f7fdd4922fc825204dc9ff62ce448f2564bcb299
Reza <mrr.reza.com>
2021-07-15 15:17:53 +0430

    generating POJOs and adding properties manager

--- src/main/java/constants/endpoints.java
+++ src/main/java/constants/endpoints.java
@@ -1,4 +1,12 @@
 package constants;
 
+import javax.print.DocFlavor;
+
 public class endpoints {
+    public static final String GET_USERS = "public/v1/users";
+    public static final String GET_SINGLE_USER = "public/v1/users/{id}";
+    public static final String CREATE_USER = "public/v1/users";
+    public static final String UPDATE_USER = "public/v1/users/{id}";
+    public static final String DELETE_USER = "public/v1/users/{id}";
+
 }
